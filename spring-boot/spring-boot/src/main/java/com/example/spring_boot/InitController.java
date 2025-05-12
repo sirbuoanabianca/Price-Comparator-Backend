67d694503ff3a8e837d8c138c3648d28c23ebf45
sirbuoanabianca <sirbuoanabianca@yahoo.com>
2025-05-12 14:20:08 +0300

    spring boot initial commit

---
+++ spring-boot/spring-boot/src/main/java/com/example/spring_boot/InitController.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InitController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
